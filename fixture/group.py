b6d2b1c349f40883705c0f9be45b8f7845f9cfae
Vladimir <Vladimir.Shkolnyy@gmail.com>
2019-05-02 21:32:57 +0300

    Create new method for fill data in groups/users

--- fixture/group.py
+++ fixture/group.py
@@ -14,15 +14,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -44,6 +36,13 @@ class GroupHelper:
         # submit edit button
         wd.find_element_by_name("edit").click()
         # fill new data group
+        self.fill_group_form(group)
+        # submit update group
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -53,9 +52,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit update group
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
--- fixture/user.py
+++ fixture/user.py
@@ -1,4 +1,4 @@
-import time
+
 
 class UserHelper:
 
@@ -10,27 +10,7 @@ class UserHelper:
         # init add new user
         wd.find_element_by_link_text("add new").click()
         # fill user form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(user.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(user.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(user.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(user.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(user.home)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(user.email)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(user.address2)
+        self.fill_user_data(user)
         # submit form
         wd.find_element_by_xpath("//input[21]").click()
 
@@ -42,14 +22,20 @@ class UserHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         # accept delete user
         wd.switch_to_alert().accept()
-        # wait auto-redirect to homepage
-        time.sleep(3)
+        # wait message
+        wd.find_elements_by_css_selector("div.msgbox")
 
     def edit_first(self, user):
         wd = self.app.wd
         # push edit button on first user
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # fill new data
+        self.fill_user_data(user)
+        # update form button
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
+
+    def fill_user_data(self, user):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(user.firstname)
@@ -71,8 +57,6 @@ class UserHelper:
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
         wd.find_element_by_name("address2").send_keys(user.address2)
-        # update form button
-        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
 
     def return_to_homepage(self):
         wd = self.app.wd
